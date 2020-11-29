b98897d773255909038359f57022c135425e3e23
Miguel Rivas <migueladanrm@outlook.com>
2020-11-28 19:21:30 -0600

    minor fixes - added mongo deps to server

--- orchestrator/Program.cs
+++ orchestrator/Program.cs
@@ -16,7 +16,7 @@ var factory = new ConnectionFactory {
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.ExchangeDeclare(exchange: "XPyxel", type: ExchangeType.Direct,durable:true);
+channel.ExchangeDeclare(exchange: "XPyxel", type: ExchangeType.Direct, durable: true);
 
 var queueName = channel.QueueDeclare().QueueName;
 channel.QueueBind(queue: queueName, exchange: "XPyxel", routingKey: "");
@@ -29,9 +29,7 @@ consumer.Received += (model, ea) => {
     var message = Encoding.UTF8.GetString(body);
     Console.WriteLine(" [x] {0}", message);
 };
-channel.BasicConsume(queue: queueName,
-                        autoAck: true,
-                        consumer: consumer);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
 
 Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
+Console.ReadLine(); 
