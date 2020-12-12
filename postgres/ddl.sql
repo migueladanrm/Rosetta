589c87f4d5bc1d30c4a007bf0e423356e4fde720
Miguel Rivas <migueladanrm@outlook.com>
2020-12-12 00:37:47 -0600

    postgres: added sql scripts

---
+++ postgres/ddl.sql
@@ -0,0 +1,98 @@
+begin;
+
+create extension if not exists "uuid-ossp";
+
+--drop table public.worker;
+create table public.worker
+(
+    id   char(8)               not null primary key,
+    name character varying(32) not null
+);
+
+--drop table public.operation;
+create table public.operation
+(
+    id          uuid                    not null primary key default uuid_generate_v4(),
+    description character varying(1024) null,
+    items       jsonb                   not null             default '[]'::jsonb,
+    filters     jsonb                   not null             default '[]'::jsonb,
+    is_done     boolean                 not null             default false,
+    creation    timestamp               not null             default now()
+);
+
+--drop table public.operation_item_task;
+create table public.operation_task
+(
+    id              uuid                  not null primary key default uuid_generate_v4(),
+    operation       uuid                  not null references public.operation (id),
+    file_id         char(24)              not null,
+    filter          character varying(24) not null,
+    assigned_worker character varying(8)  null references public.worker (id),
+    assigned_at     timestamp             null,
+    finished_at     timestamp             null,
+    output_file     char(24)              null
+);
+
+-- TRIGGERS
+
+create or replace function register_operation_tasks() returns trigger
+    language plpgsql as
+$$
+declare
+    _items   jsonb;
+    _filters jsonb;
+    _i       jsonb;
+    _f       character varying(24);
+begin
+    select items into _items from operation where id = NEW.id limit 1;
+    select filters into _filters from operation where id = NEW.id limit 1;
+
+    for _i in select jsonb_array_elements from jsonb_array_elements(_items)
+        loop
+            for _f in select jsonb_array_elements from jsonb_array_elements(_filters)
+                loop
+                    insert into operation_task(operation, file_id, filter) values (NEW.id, (select _i ->> 'id'), _f);
+                end loop;
+        end loop;
+
+    return new;
+end;
+$$;
+
+create trigger tr_register_operation_tasks
+    after insert
+    on public.operation
+    for each row
+execute procedure register_operation_tasks();
+
+
+create or replace function update_operation_status() returns trigger
+    language plpgsql as
+$$
+declare
+    operation_id    uuid;
+    done            bool := true;
+    tmp_finished_at timestamp;
+begin
+    select operation into operation_id from operation_task where operation = new.operation limit 1;
+
+    for tmp_finished_at in select finished_at from operation_task where operation = operation_id
+        loop
+            if tmp_finished_at is null then
+                done := false;
+            end if;
+        end loop;
+
+    update operation set is_done = done where id = operation_id;
+
+    return new;
+end;
+$$;
+
+create trigger tr_update_operation_status
+    after update
+    on public.operation_task
+    for each row
+execute procedure update_operation_status();
+
+end;
---
+++ postgres/dml.sql
@@ -0,0 +1,3 @@
+insert into worker(id, name)
+values ('pyxel-01', 'Nodo 1'),
+       ('pyxel-02', 'Nodo 2');
